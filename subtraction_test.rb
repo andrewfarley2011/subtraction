d195b1800a6487560fde83419d98b561f4aa9aa1
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-07 10:36:22 -0400

    Subtraction Test B

--- subtraction_test.rb
+++ subtraction_test.rb
@@ -1,9 +1,27 @@
 
+require "minitest/autorun"
+
 require_relative "subtraction.rb"
 
-def sum_total(x,y)
-	x - y
+class Addition < Minitest::Test
+
+	def test_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_returns_4
+		assert_equal(4,sum_total(10,6))
+	end
+
+	def test_returns_5
+		assert_equal(5,sum_total(10,5))
+	end
+
 end
+
+# def sum_total(x,y)
+# 	x - y
+# end
 
 # puts sum_total(10,5)
 
